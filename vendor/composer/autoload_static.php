578e05b5599b8d27fc4c10e229d5a97da321b0b9
jvvppereira <joao.paes@philips.com>
2018-06-19 20:09:28 -0300

    changes to load files in s3

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,47 @@ namespace Composer\Autoload;
 
 class ComposerStaticInite4bdcf804d7d4f61a8f8ccae82602f04
 {
+    public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'Symfony\\Component\\EventDispatcher\\' => 34,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Symfony\\Component\\EventDispatcher\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/event-dispatcher',
+        ),
+    );
+
+    public static $prefixesPsr0 = array (
+        'G' => 
+        array (
+            'Guzzle\\Tests' => 
+            array (
+                0 => __DIR__ . '/..' . '/guzzle/guzzle/tests',
+            ),
+            'Guzzle' => 
+            array (
+                0 => __DIR__ . '/..' . '/guzzle/guzzle/src',
+            ),
+        ),
+        'A' => 
+        array (
+            'Aws' => 
+            array (
+                0 => __DIR__ . '/..' . '/aws/aws-sdk-php/src',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInite4bdcf804d7d4f61a8f8ccae82602f04::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInite4bdcf804d7d4f61a8f8ccae82602f04::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInite4bdcf804d7d4f61a8f8ccae82602f04::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
