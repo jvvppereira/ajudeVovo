5802cae47c123f44f259157f1f0f2fe6e4c15b45
João Vítor <joao.paes@philips.com>
2018-06-18 21:17:13 -0300

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -71,7 +71,7 @@ self.addEventListener('fetch', function(event) {
             // and because we want the browser to consume the response
             // as well as the cache consuming the response, we need
             // to clone it so we have two streams.
-            var responseToCache = undefined;
+            var responseToCache = null;
             // response.clone();
 
             caches.open(dataCacheName)
