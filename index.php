be483cbc7cdb4e10e81d4dcd6520213ab7088754
jvvppereira <joao.paes@philips.com>
2018-06-20 12:09:35 -0300

    first test to load s3

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+require('vendor/autoload.php');
+// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+$s3 = Aws\S3\S3Client::factory();
+$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+
+/*
+example to upload a file, more details:
+https://devcenter.heroku.com/articles/s3-upload-php#handling-file-uploads
+
+$upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+
+htmlspecialchars($upload->get('ObjectURL'))
+*/
+
 //echo phpinfo();
 
 /**
