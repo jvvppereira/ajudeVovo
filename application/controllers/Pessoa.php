1a29b96bb4e213a0c624805512d8a02163f7d0b4
João Vítor <joao.paes@philips.com>
2018-06-19 23:22:02 -0300

    Update Pessoa.php

--- application/controllers/Pessoa.php
+++ application/controllers/Pessoa.php
@@ -112,7 +112,7 @@
 
 	      	// Salva a imagem no servidor e dpois registra o caminho no banco
 				if ( !empty($arrDadosImagem['urlFoto']) ) {				
-					print_r('imagem 1--');
+					//print_r('imagem 1--');
 					$ds_imagem = $this->salvarImagemPessoa(
 						$arrDadosImagem,
 						$cd_pessoa
@@ -292,7 +292,9 @@
 			}
 
 			// limpar o buffer de imagem
-			ob_clean(); 
+			if (ob_get_length()) {
+				ob_clean(); 
+			}  
 			ob_start();
 			if ( $extensao == 'png' ){
 				$img_r = imagecreatefrompng($ds_imagem);				
